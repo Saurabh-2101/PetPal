4d59f8fc764c4dc863666f6e5d3351c6eff62dfe
Saurabh-2101 <124668908+Saurabh-2101@users.noreply.github.com>
2023-08-03 09:41:31 +0530

    Add files via upload

---
+++ connection/connect.php
@@ -0,0 +1,18 @@
+<?php
+ $servername = "Localhost";
+ $username = "root";
+ $password = "";
+ $dbname = "demo";
+
+
+ // Create connection
+ $db = mysqli_connect("$servername","$username","$password","$dbname");
+  //Check connection
+ if (!$db)
+    {
+    die("Connection failed: " . mysqli_connect_error());
+    }
+
+
+
+?>
